07e07e838c2b28070e62046760621d359ea06dbc
leemirim <leemirim9654@gmail.com>
2019-03-15 03:11:37 +0900

    Dockerfile로 Docker 이미지 생성 및 run해보기

--- simpleapp/simpleapp/settings/prod.py
+++ simpleapp/simpleapp/settings/prod.py
@@ -1,7 +1,7 @@
 from .common import *
 
-STATICFILES_STORAGE = 'askdjango.storages.StaticAzureStorage'
-DEFAULT_FILE_STORAGE = 'askdjango.storages.MediaAzureStorage'
+STATICFILES_STORAGE = 'simpleapp.storages.StaticAzureStorage'
+DEFAULT_FILE_STORAGE = 'simpleapp.storages.MediaAzureStorage'
 
 AZURE_ACCOUNT_NAME = os.environ.get('AZURE_ACCOUNT_NAME')
 AZURE_ACCOUNT_KEY = os.environ.get('AZURE_ACCOUNT_KEY')
--- simpleapp/simpleapp/urls.py
+++ simpleapp/simpleapp/urls.py
@@ -2,12 +2,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.shortcuts import render
-from django.urls import path
+from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', lambda request: render(request, 'root.html'), name='root'),
+    path('accounts/', include('accounts.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
